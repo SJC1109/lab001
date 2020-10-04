cacf10b92efedaf7ea3f9798ad8cfcbe27bcb966
Jiacheng Shen <1449924798@qq.com>
2020-10-04 15:04:51 +0100

    Arrays

--- fundamentals.js
+++ fundamentals.js
@@ -1,5 +1,10 @@
 const aCar = {
     owner: "Joe Bloggs",
+    address : '3 Walkers Lane',
+    previous_owners: [
+        { name: "Pat Smith", address: "1 Main Street" },
+        { name: "Sheila Dwyer", address: "2 High Street" }
+      ],
     type: {
       make: "Toyota",
       model: "Corolla",
@@ -31,4 +36,10 @@ console.log(
     " milage, with " +
     aCar.color.interior.texture +
     " interior."
-);
+)
+console.log(
+    "First owner: " +
+      aCar.previous_owners[0].name +
+      ' - ' +
+      aCar.previous_owners[0].address
+  );
