a7ce62be7b08b3047fd4b631907b51a253e2d173
Jiacheng Shen <1449924798@qq.com>
2020-10-04 15:16:29 +0100

    Loop constructs

--- fundamentals.js
+++ fundamentals.js
@@ -10,6 +10,7 @@ const aCar = {
       model: "Corolla",
       cc: 1.8
     },
+    features : ['Parking assist', 'Alarm', 'Tow-bar'],
     registration: { year: 201, county: "WD", number: 1058 }
   };
   
@@ -43,3 +44,9 @@ console.log(
       ' - ' +
       aCar.previous_owners[0].address
   );
+  for (let i = 0 ; i < aCar.features.length ; i += 1) {
+    console.log(aCar.features[i]) ;
+} 
+  for (let i = 0 ; i < aCar.previous_owners.length ; i += 1) {
+    console.log(aCar.previous_owners[i].name);
+}
